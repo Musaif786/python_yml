2620d3ca293a374afdc0859176a5bc13c4b5df9e
Musaif <mdmmusaif.mm@gmail.com>
2022-07-29 15:28:17 +0530

    added test4.yml file

--- test2.py
+++ test2.py
@@ -1,10 +1,11 @@
 import yaml
 import pprint as pp
 
-with open('test3.yml','r') as files:
+with open('test4.yml','r') as files:
     parse = yaml.safe_load(files)
 
 #pp.pprint(parse['person']['details'])
 #pp.pprint(parse['person3'])
-a = input("Enter, what you want to extract : \n")
-pp.pprint(parse[a])
+#a = input("Enter, what you want to extract : \n")
+#pp.pprint(parse[a])
+pp.pprint(parse['employees2'])
